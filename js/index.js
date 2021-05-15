e6cb369881c0d15bb3d4baa5ea77ad971829adbc
Yosman Andres Higuita Gonzalez <yosman.higuita028@gmail.com>
2021-05-15 13:46:48 -0500

    Agregando nuevas funcionalidades al slider y finalizando la seccion de nosotros

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@
 const slides = document.querySelectorAll('.slider__item');
 const header = document.querySelector('.header');
 const nav = document.querySelector('.nav');
+const nav__list = document.querySelectorAll('.nav__list');
+const nav__link = document.querySelectorAll('.nav__link');
+const sections = document.querySelectorAll('.section');
+const sliderDotContainer = document.querySelector('.slider__dot');
 
 // BOTONES
 const btnRight = document.querySelector('.slider__btn--right');
@@ -17,8 +21,25 @@ const goToSlide = function (slide) {
     el.style.transform = `translateX(${100 * (i - slide)}%)`;
   });
 };
-goToSlide(0);
-// 0%, 100%, 200%, 300%
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    const html = `
+    <div class="slider__dot-item" data-slide="${i}"></div>
+    `;
+    sliderDotContainer.insertAdjacentHTML('beforeend', html);
+  });
+};
+
+const activeDots = function (slide) {
+  document
+    .querySelectorAll('.slider__dot-item')
+    .forEach(s => s.classList.remove('slider__dot-item--active'));
+
+  document
+    .querySelector(`.slider__dot-item[data-slide="${slide}"]`)
+    .classList.add('slider__dot-item--active');
+};
 
 const nextSlide = function () {
   if (curSlide === maxSlide - 1) {
@@ -27,6 +48,7 @@ const nextSlide = function () {
     curSlide++;
   }
   goToSlide(curSlide);
+  activeDots(curSlide);
 };
 
 const prevSlide = function () {
@@ -36,7 +58,15 @@ const prevSlide = function () {
     curSlide--;
   }
   goToSlide(curSlide);
+  activeDots(curSlide);
 };
+
+const init = function () {
+  goToSlide(0);
+  createDots();
+  activeDots(0);
+};
+init();
 
 let timerSlide = setInterval(nextSlide, 10000);
 const resetTimerSlide = function () {
@@ -45,6 +75,7 @@ const resetTimerSlide = function () {
 };
 
 // Sticky navigation and show btn up
+const heightNav = nav.getBoundingClientRect().height;
 const headerObserver = new IntersectionObserver(
   function (entries) {
     const [entry] = entries;
@@ -59,9 +90,28 @@ const headerObserver = new IntersectionObserver(
   {
     root: null,
     threshold: 0,
+    rootMargin: `-${heightNav}px`,
   }
 );
 headerObserver.observe(header);
+
+const sectionObserver = new IntersectionObserver(
+  function (entries, observer) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  },
+  {
+    root: null,
+    threshold: 0.15,
+  }
+);
+
+sections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 // EVENTS
 btnRight.addEventListener('click', function () {
@@ -74,4 +124,43 @@ btnLeft.addEventListener('click', function () {
 });
 btnUp.addEventListener('click', function () {
   document.querySelector('header').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') prevSlide();
+  if (e.key === 'ArrowRight') nextSlide();
+});
+
+sliderDotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('slider__dot-item')) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activeDots(slide);
+    resetTimerSlide();
+  }
+});
+
+// Page links navigation
+const paddingSection = window
+  .getComputedStyle(sections[0])
+  .getPropertyValue('padding-top');
+
+console.log(paddingSection);
+nav__list.forEach(function (list) {
+  list.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (e.target.classList.contains('nav__link')) {
+      const id = e.target.getAttribute('href');
+      const coordenadas = document.querySelector(id).getBoundingClientRect();
+
+      window.scrollTo({
+        top:
+          coordenadas.top -
+          Number.parseInt(paddingSection) +
+          window.pageYOffset,
+        left: coordenadas.left + window.pageXOffset,
+        behavior: 'smooth',
+      });
+    }
+  });
 });
