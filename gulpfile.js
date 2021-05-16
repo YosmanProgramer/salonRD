316790cb35f4261213a7cddc7839c4aa6e2b85a7
Yosman Andres Higuita Gonzalez <yosman.higuita028@gmail.com>
2021-05-16 18:19:00 -0500

    Implementando responsive web design primera version

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
+const minifyJs = require('gulp-uglify');
 
 exports.sass = done => {
   return src('./sass/**/*.scss')
@@ -9,10 +10,14 @@ exports.sass = done => {
     .pipe(dest('./css'));
 };
 
-exports.minificar = done => {
+exports.minificarCss = done => {
   return src('./sass/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(dest('./css'));
+};
+
+exports.minificarJs = done => {
+  return src('./js/**/*.js').pipe(minifyJs()).pipe(dest('./js'));
 };
 
 exports.default = done => {
